0b38dc0cebbefe95297ae69e67e59099b9ee4116
Regina <regescmar1@alum.us.es>
2021-10-28 07:47:32 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'master' into regescmar1

--- src/main/java/org/springframework/samples/petclinic/usuario/UsuarioController.java
+++ src/main/java/org/springframework/samples/petclinic/usuario/UsuarioController.java
@@ -1,17 +1,22 @@
 package org.springframework.samples.petclinic.usuario;
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UsuarioController {
-	
+	private static final String VIEWS_USUARIO_CREATE_OR_UPDATE_FORM = "usuarios/createOrUpdateUsuarioForm";
 	@Autowired
 	private UsuarioService usuarioService;
 	
@@ -29,4 +34,25 @@ public class UsuarioController {
 		mav.addObject(this.usuarioService.findUsuarioById(usuarioId));
 		return mav;
 	}
+	
+	@GetMapping(value = "/usuarios/{usuarioId}/edit")
+	public String initUpdateUsuarioForm(@PathVariable("usuarioId") int usuarioId, Model model) {
+		Usuario usuario = this.usuarioService.findUsuarioById(usuarioId);
+		model.addAttribute(usuario);
+		return VIEWS_USUARIO_CREATE_OR_UPDATE_FORM;
+	}
+	
+	@PostMapping(value = "/usuarios/{usuarioId}/edit")
+	public String processUpdateUsuarioForm(@Valid Usuario usuario, BindingResult result,
+			@PathVariable("usuarioId") int usuarioId) {
+		if (result.hasErrors()) {
+			return VIEWS_USUARIO_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			usuario.setId(usuarioId);
+			this.usuarioService.saveUsuario(usuario);
+			return "redirect:/usuarios/{usuarioId}";
+		}
+	}
+
 }
--- src/main/java/org/springframework/samples/petclinic/usuario/UsuarioService.java
+++ src/main/java/org/springframework/samples/petclinic/usuario/UsuarioService.java
@@ -1,10 +1,10 @@
 package org.springframework.samples.petclinic.usuario;
 
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.samples.petclinic.owner.Owner;
-import org.springframework.samples.petclinic.owner.OwnerRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,28 +12,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class UsuarioService {
 	@Autowired
 	private UsuarioRepository usuarioRepo;
+
 	
 	@Autowired
 	public UsuarioService(UsuarioRepository usuarioRepo) {
 		this.usuarioRepo = usuarioRepo;
 	}	
-	
-	@Transactional
-	public int usuarioCount() {
-		return (int)	usuarioRepo.count();
-	}
-	
+
+//	@Transactional
+	//public int usuarioCount() {
+		//return (int)	usuarioRepo.count();
+	//}
+
 	@Transactional
 	public Iterable<Usuario> findAll() {
 		return usuarioRepo.findAll();
-		
+
 	}
-	
-	
-	
+
+
+
 	@Transactional(readOnly = true)
 	public Usuario findUsuarioById(int id) throws DataAccessException {
 		return usuarioRepo.findById(id);
 	}
-	
+
+	@Transactional
+	public void saveUsuario(Usuario usuario) throws DataAccessException {
+		usuarioRepo.save(usuario);	
+	}
 }
