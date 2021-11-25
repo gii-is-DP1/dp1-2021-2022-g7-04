d53072a104fda9dc4a94074a696220c1acaf0c0d
Jose Manuel <joslobtro@alum.us.es>
2021-11-25 21:35:14 +0100

    Test realizados

--- src/test/java/org/springframework/samples/buscaminas/service/UserServiceTest.java
+++ src/test/java/org/springframework/samples/buscaminas/service/UserServiceTest.java
@@ -30,5 +30,40 @@ public class UserServiceTest {
 		jugadores = this.userService.findPlayersByUsername("jugador0");
 		assertThat(jugadores.isEmpty()).isTrue();
 	}
+	
+	@Test
+	void shouldDeletePlayer() {
+		Collection<User> jugadores = this.userService.findPlayersByUsername("");
+		int size1 = jugadores.size();
+		this.userService.deleteUser("jugador1");
+		
+		Collection<User> jugadores2 = this.userService.findPlayersByUsername("");
+		assertThat(jugadores2.size()).isNotEqualTo(size1);
+	}
+	
+	@Test
+	void shouldCreateUser() {
+		User u = new User();
+		u.setUsername("jose");
+		u.setPassword("jose123");
+		u.setEnabled(true);
+		this.userService.saveUser(u);
+		
+		User u2 = this.userService.findByUsername("jose");
+		assertThat(u.getUsername()).isEqualTo(u2.getUsername());
+		
+	}
+	
+	@Test
+	void shouldUpdateUser() {
+		User u = this.userService.findByUsername("jugador1");
+		String pass = u.getPassword();
+		String newPass = pass + "321";
+		u.setPassword(newPass);
+		this.userService.saveUser(u);
+		
+		User u2 = this.userService.findByUsername("jugador1");
+		assertThat(u2.getPassword()).isEqualTo(newPass);
+	}
 
 }
