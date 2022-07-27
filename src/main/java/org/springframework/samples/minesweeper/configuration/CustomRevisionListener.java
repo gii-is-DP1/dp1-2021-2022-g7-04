3a13acd9d88a7bae0c1f130469b0c389f71d4e37
danmarsua1 <djmartinezsuarez91@gmail.com>
2022-07-27 16:45:42 +0200

    add player audits with Hibernate-Envers. Pending also audit player deletions (not implemented yet)

---
+++ src/main/java/org/springframework/samples/minesweeper/configuration/CustomRevisionListener.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.minesweeper.configuration;
+
+import org.hibernate.envers.RevisionListener;
+import org.springframework.samples.minesweeper.audit.Revision;
+
+public class CustomRevisionListener implements RevisionListener {
+	
+	public void newRevision(Object revisionEntity) {
+		final Revision revision = (Revision) revisionEntity;
+	}
+
+}
